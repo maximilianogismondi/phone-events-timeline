428647889feceeb6ff09f872123589f8cde2c1cb
Maximiliano Gismondi <maximiliano.gismondi@nubank.com.br>
2021-03-16 13:09:38 -0300

    Indentation

--- timeline.js
+++ timeline.js
@@ -162,7 +162,7 @@ const generateActorRows = function(actorEvents) {
   console.log(actorEvents);
   return actorEvents.events.map((e, i) => {
     if (actorEvents.events.length > (i+1) && !isFinalEvent(e)) {
-    	console.log(e.twilio_event_type);
+      console.log(e.twilio_event_type);
       let nextEvent = actorEvents.events[i+1];
       let eventType;
       if (actorEvents.provider === "twilio") {
